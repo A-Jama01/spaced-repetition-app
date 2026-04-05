ea43d2d73fe031b8d1c1fd6e1175597817144ff3
A-Jama01 <abdurahmanjama@cmail.carleton.ca>
2026-04-05 01:01:44 -0400

    feat: Create Heatmap for frontend

--- frontend/src/components/StatsView.tsx
+++ frontend/src/components/StatsView.tsx
@@ -2,6 +2,12 @@ import fetchStats from "@/api/statsAPI";
 import { useQuery } from "@tanstack/react-query";
 import CalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
+import { Tooltip } from "react-tooltip";
+
+interface HeatMapCell {
+  date: Date;
+  count: number;
+}
 
 export default function StatsView() {
   const { isPending, isError, data, error } = useQuery({
@@ -16,7 +22,61 @@ export default function StatsView() {
     return <span>Error: {error.message}</span>;
   }
 
-  console.log(data.stats);
+  // (AJama) Get values to populate heatmap
+  const YEAR_SIZE = 366;
+  const currentDate = new Date();
+  const startDate = new Date(currentDate);
+  startDate.setFullYear(startDate.getFullYear() - 1);
+
+  const heatmapMap = new Map();
+  for (let i = 0; i < YEAR_SIZE; i++) {
+    const nextDate = new Date(currentDate);
+    nextDate.setDate(nextDate.getDate() - i);
+    heatmapMap.set(nextDate, 0);
+  }
+
+  for (let i = 0; i < data.stats.heatmap.length; i++) {
+    const heatmapCellDate = new Date(data.stats.heatmap[i].review_date);
+    heatmapMap.set(heatmapCellDate, data.stats.heatmap[i].reviews);
+  }
+
+  const heatmapValues: HeatMapCell[] = [];
+  heatmapMap.forEach((value: number, key: Date) => {
+    heatmapValues.push({
+      date: key,
+      count: value,
+    });
+  });
+
+  const maxHeatmapValue = Math.max(...heatmapValues.map((cell) => cell.count));
+
+  function classForValue(
+    value:
+      | CalendarHeatmap.ReactCalendarHeatmapValue<CalendarHeatmap.ReactCalendarHeatmapDate>
+      | undefined,
+  ): string {
+    if (!value || value.count === 0) {
+      return "color-scale-0";
+    }
+
+    const level = Math.ceil((value.count / maxHeatmapValue) * 4);
+    return `color-scale-${level}`;
+  }
+
+  function getTooltipDataAttrs(
+    value:
+      | CalendarHeatmap.ReactCalendarHeatmapValue<CalendarHeatmap.ReactCalendarHeatmapDate>
+      | undefined,
+  ): any {
+    if (!value) return null;
+
+    const date = new Date(value.date).toLocaleDateString();
+
+    return {
+      "data-tooltip-id": "heatmap-tooltip",
+      "data-tooltip-content": `${date}: ${value.count} reviews`,
+    };
+  }
 
   return (
     <div className="flex flex-1 flex-col">
@@ -36,7 +96,17 @@ export default function StatsView() {
       <h1 className="scroll-m-20 text-2xl text-center font-semibold tracking-tight">
         Heatmap
       </h1>
-      <div className="flex flex-row justify-evenly"></div>
+      <div className="flex flex-row">
+        <CalendarHeatmap
+          startDate={startDate}
+          endDate={currentDate}
+          values={heatmapValues}
+          showWeekdayLabels={true}
+          classForValue={classForValue}
+          tooltipDataAttrs={getTooltipDataAttrs}
+        />
+        <Tooltip id="heatmap-tooltip" />
+      </div>
     </div>
   );
 }
